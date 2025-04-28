fada555aa1f7e1632676dd4abb2598a3ac039409
Richelieau <abiyyudzakiraghib@gmail.com>
2025-04-28 08:51:04 +0800

    Add files via upload

---
+++ bukhoriquiz.php
@@ -0,0 +1,10 @@
+<?php
+$nilaiMatematika = 65;
+$nilaiBahasa = 55;
+
+if ($nilaiMatematika >= 80 && $nilaiBahasa>= 50) {
+    echo "Selamat ! Kamu boleh mabar epep akhir pekan ini";}
+    else {
+        echo "Yah ! Belajar lebih giat lagi yahh";
+}
+

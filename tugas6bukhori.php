7faf06fd743da8c23c3eba117fd82600a537b72f
Richelieau <abiyyudzakiraghib@gmail.com>
2025-04-29 14:28:10 +0800

    tugas

---
+++ tugas6bukhori.php
@@ -0,0 +1,13 @@
+<?php
+$kunsya = 94;
+
+if ($kunsya >= 90) {
+    echo "kamu lulus";
+} elseif ($kunsya >= 80) {
+    echo "baik";
+} elseif ($kunsya >= 75) {
+    echo "cukup";
+} else {
+    echo "tidak mampu";
+}
+?>
